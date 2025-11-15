b2a9353c8965a0d2a3c801c5b5dc3237c14f5787
killer7wayes-lab <riadhaouas@gmail.com>
2025-11-16 00:38:58 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-"use client";
 import TarotApp from "./TarotApp";
 
 export default function Page() {
-  return <TarotApp />;
+  return (
+    <main className="min-h-screen p-10 bg-[#0B0A16] text-white">
+      <TarotApp />
+    </main>
+  );
 }
